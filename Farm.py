cf22b3fdb38c2c4ebc78117111d7852e887c1ad2
TedMcFerg <tedmcferg@gmail.com>
2024-08-26 16:32:12 -0400

    Update Farm.py

--- Farm.py
+++ Farm.py
@@ -1,11 +1,22 @@
 from Model_Classes import Animals
 
 class Farm:
-  def __init__(self, title, funds):
-    self.name = title
-    self.money = funds
-    self.number_animals=0
-    self.food = self.money *5
-    self.animals = [Animals.Animal(10, 2, 7), Animals.Animal(1, 5, 15), Animals.Animal(5, 7, 25)]
-    self.description = title + " Farm"
+    def __init__(self):
+        self.food = 100
+        self.animals = []
 
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def remove_animal(self, animal):
+        self.animals.remove(animal)
+
+    def simulate_day(self):
+        for animal in self.animals:
+            animal.grow()
+            if self.food > 0:
+                animal.eat()
+            else:
+                print(f"Not enough food for {animal.__class__.__name__}!")
+                animal.health -= 10
+            animal.check_health()
